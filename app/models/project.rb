63a802be170f4f3c213a50a105a2a60d7c80b44a
Pawel Parafianowicz <pawelp@outlook.com>
2017-02-22 17:02:57 -0500

    Create project owner functionality, tested in console

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,8 +1,8 @@
 class Project < ActiveRecord::Base
+  belongs_to :user
   has_many :rewards
   has_many :pledges, through: :rewards
   has_many :backers, through: :rewards
   accepts_nested_attributes_for :rewards
   validates_presence_of :title, :description, :goal, :start_date, :end_date
-
 end
