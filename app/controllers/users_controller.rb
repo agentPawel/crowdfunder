990b3ce2ad61a5a0ce14fe52bbbee6b1b4d11ccb
Pawel Parafianowicz <pawelp@outlook.com>
2017-02-23 17:06:26 -0500

    Reward and pledges to show on user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @projects_owned = current_user.owned_projects
-    @projects_title = []
     @pledges_made = []
     @pledges_made = current_user.pledges
   end
